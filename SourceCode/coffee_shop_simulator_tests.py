4b91a2d170af0735d565df86d763b25715b5cdc9
Ryan <246ryan@gmail.com>
2025-06-21 15:26:08 -0700

    Add files via upload

---
+++ SourceCode/coffee_shop_simulator_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from coffee_shop_simulator import CoffeeShopSimulator
+
+class CoffeeShopSimulatorTests(unittest.TestCase):
+    def test_convert_to_float(self):
+        # Test a string conversion to float
+        test_float = 1.23
+        test_string = "1.23"
+
+if __name__ == '__main__':
+    unittest.main()
